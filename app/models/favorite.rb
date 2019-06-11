a415cc724146643a1628def4ba06261357a5733a
Raghu Betina <raghu@firstdraft.com>
2019-06-11 04:29:55 +0000

    Direct association between restaurants and favorites

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -1,6 +1,9 @@
 class Favorite < ApplicationRecord
   # Direct associations
 
+  belongs_to :restaurant,
+             :counter_cache => true
+
   belongs_to :user,
              :counter_cache => true
 
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,6 +1,9 @@
 class Restaurant < ApplicationRecord
   # Direct associations
 
+  has_many   :favorites,
+             :dependent => :destroy
+
   # Indirect associations
 
   # Validations
