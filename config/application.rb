3483db2ef965f8ff43fa7d1d1fcd1ac2223b1ef4
Raghu Betina <raghu@firstdraft.com>
2019-06-11 04:29:01 +0000

    Install rspec

--- config/application.rb
+++ config/application.rb
@@ -18,6 +18,17 @@ Bundler.require(*Rails.groups)
 
 module RestaurantRecs
   class Application < Rails::Application
+
+      config.generators do |generate|
+        generate.helper false
+        generate.javascripts false
+        generate.request_specs false
+        generate.routing_specs false
+        generate.stylesheets false
+        generate.test_framework :rspec
+        generate.view_specs false
+      end
+
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 
