4b66783e22c04aba33590bd87fa099a1386d5200
Raghu Betina <raghu@firstdraft.com>
2019-06-11 04:30:08 +0000

    run rails g active_admin:install

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
   root :to => "favorites#index"
   # Routes for the Comment resource:
 
