aa422900cef048318680df721c718b5ab842bbd0
Raghu Betina <raghu@firstdraft.com>
2019-06-11 04:30:29 +0000

    Whitelist columns in Cuisine for ActiveAdmin

--- app/admin/cuisines.rb
+++ app/admin/cuisines.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register Cuisine do
+
+ permit_params :name
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
